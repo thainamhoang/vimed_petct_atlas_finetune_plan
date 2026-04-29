d4480cce5f24c82f2fc818749964b3434c866540
thainamhoang <namhoangthai2607@gmail.com>
2026-04-29 16:25:31 +0700

    patch MultilabelBCELoss

--- scripts/apply_pillar_finetune_patch.sh
+++ scripts/apply_pillar_finetune_patch.sh
@@ -11,6 +11,7 @@ PILLAR_ROOT="$2"
 
 cp "${PLAN_ROOT}/pillar_finetune_patch/pillar/datasets/vimed_petct.py" "${PILLAR_ROOT}/pillar/datasets/"
 cp "${PLAN_ROOT}/pillar_finetune_patch/pillar/datasets/__init__.py" "${PILLAR_ROOT}/pillar/datasets/"
+cp "${PLAN_ROOT}/pillar_finetune_patch/pillar/engines/base.py" "${PILLAR_ROOT}/pillar/engines/"
 cp "${PLAN_ROOT}/pillar_finetune_patch/pillar/engines/classifier.py" "${PILLAR_ROOT}/pillar/engines/"
 cp "${PLAN_ROOT}/pillar_finetune_patch/pillar/models/multi_stage.py" "${PILLAR_ROOT}/pillar/models/"
 cp "${PLAN_ROOT}/pillar_finetune_patch/pillar/models/backbones/mmatlas.py" "${PILLAR_ROOT}/pillar/models/backbones/"
